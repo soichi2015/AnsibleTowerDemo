ae0011a3f1f85e81add5994be160cf0f0338427d
Soichiro Anzai <soichi@r6.dion.ne.jp>
2016-06-21 12:30:17 +0900

    aaa

--- wintest/files/get-installed.ps1
+++
@@ -1,5 +0,0 @@
-Param($csvpath)
-Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall |
- foreach {Get-ItemProperty $_.PSPath} |
- select DisplayName,DisplayVersion,InstallDate,Publisher |
- export-csv $csvpath -notype
